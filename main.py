2699137bd087710dd4125448d9e6859410790f37
Julia Paiva Braga <julia.p.b@dinfor.uniceub>
2026-08-27 21:06:30 -0300

    Corrige limite de aprovação

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def calcular_media(notas: list[float])->float:
 
 def situacao_aluno(media: float)-> str:
     """Classificaoalunoapartirdesuamédiafinal."""
-    if media > 7:
+    if media >= 7:
         return "Aprovado"
     if media >= 5:
         return "Recuperação"
